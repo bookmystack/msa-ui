567f8a6f300f18f4f665ccf3db9a8d8aea49fd0b
Rajat Tarade C137 <rtarade@gmail.com>
2025-06-08 20:05:37 +0530

    Clean Landing Page

---
+++ src/app/pages/landingpage/section/navigation-section/navigation-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation-section.component.html',
+  styleUrl: './navigation-section.component.css'
+})
+export class NavigationSectionComponent {
+
+}
